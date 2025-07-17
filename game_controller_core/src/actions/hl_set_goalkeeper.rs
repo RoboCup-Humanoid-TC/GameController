b3ce683936dce2e0c09fb409226156a490b62fbc
Arne Hasselbring <arha@uni-bremen.de>
2025-07-17 11:26:39 +0200

    Remove debug print

--- game_controller_core/src/actions/hl_set_goalkeeper.rs
+++ game_controller_core/src/actions/hl_set_goalkeeper.rs
@@ -16,10 +16,6 @@ pub struct HlSetGoalkeeper {
 impl Action for HlSetGoalkeeper {
     fn execute(&self, c: &mut ActionContext) {
         c.game.teams[self.side].goalkeeper = Some(self.player);
-        println!(
-            "DEBUG: Goalkeeper: {:?}, Playernumber: {:?}\nSide: {:?}",
-            c.game.teams[self.side].goalkeeper, self.player, self.side
-        );
     }
 
     fn is_legal(&self, c: &ActionContext) -> bool {
